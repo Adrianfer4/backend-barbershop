3c82415f13d9f4a37130224f58124d9c8c03cb6b
Adrianfer4 <yerssondereck@gmail.com>
2026-02-02 18:38:19 -0500

    Fix: port cors

--- src/index.js
+++ src/index.js
@@ -12,17 +12,17 @@ import citasRoutes from "./routes/citasRoutes.js";
 import ingresosRoutes from "./routes/ingresosRoutes.js";
 dotenv.config();
 
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_PORT:', process.env.DB_PORT);
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_NAME:', process.env.DB_NAME);
-console.log('DATABASE_URL:', process.env.DATABASE_URL);
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
 
 const app = express();
 app.use(
   cors({
     origin: [
-      "http://localhost:3000",
+      "http://localhost:5173",
       "https://frontend-barbershop-kappa.vercel.app",
     ],
     credentials: true,
