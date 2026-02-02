366d48a9ec48c33e1fae92140a9e3b7cafb9b9d5
Adrianfer4 <yerssondereck@gmail.com>
2026-02-02 14:00:36 -0500

    Update config for Railway deployment

--- src/config/db.js
+++ src/config/db.js
@@ -4,12 +4,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   waitForConnections: true,
-  connectionLimit: parseInt(process.env.DB_CONNECTION_LIMIT) || 10,
+  connectionLimit: 10,
   queueLimit: 0
 });
 
