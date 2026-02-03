4213507ed773afa413de07754a94f30fdcb90b16
Adrianfer4 <yerssondereck@gmail.com>
2026-02-03 11:36:56 -0500

    fix de cors

--- src/config/db.js
+++ src/config/db.js
@@ -3,9 +3,4 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_PORT:', process.env.DB_PORT);
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_NAME:', process.env.DB_NAME);
-
 export const pool = mysql.createPool(process.env.DATABASE_URL);
